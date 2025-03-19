3611e25d48aa0b84d5800888b161ac0850cb0313
ayer <1374590167@qq.com>
2025-03-19 18:30:42 +0800

    learn mutex

---
+++ Thead/Thread_instance_call_once_Log.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+
+
+using namespace std;
+
+class Log{
+    public:
+    Log(const Log& log) = delete;//ban copy constructor.
+
+    private:
+
+
+
+};
+
+
+int main(){
+
+
+    return 0;
+}
